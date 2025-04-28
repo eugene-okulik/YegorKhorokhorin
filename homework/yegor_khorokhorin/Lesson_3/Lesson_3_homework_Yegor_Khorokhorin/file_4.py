ebeb8b46a18c55b9ef77d98d19c293dc2895ac9a
Yegor55 <khorokhorinyegor@gmail.com>
2025-04-28 17:29:31 +0200

    4 files with answers are added to homework_3

--- homework/yegor_khorokhorin/Lesson_3/Lesson_3_homework_Yegor_Khorokhorin/file_4.py
+++ homework/yegor_khorokhorin/Lesson_3/Lesson_3_homework_Yegor_Khorokhorin/file_4.py
@@ -3,6 +3,6 @@ a = 2
 b = 3
 hypotenuse = (math.sqrt(a**2 + b**2))
 hypotenuse = round(hypotenuse, 2)
-square = int(a*b/2)
+square = int(a * b / 2)
 print("Гипотенуза:", hypotenuse)
 print("Площадь:", square)
