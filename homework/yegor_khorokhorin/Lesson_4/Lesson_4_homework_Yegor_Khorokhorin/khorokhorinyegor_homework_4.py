b772ab0ef376cf14af37459346b7a112d70fffb0
Yegor55 <khorokhorinyegor@gmail.com>
2025-04-30 08:58:18 +0200

    1 file with fixed answers (Fixed key: now it is a tuple of one element) is added to homework_4

--- homework/yegor_khorokhorin/Lesson_4/Lesson_4_homework_Yegor_Khorokhorin/khorokhorinyegor_homework_4.py
+++ homework/yegor_khorokhorin/Lesson_4/Lesson_4_homework_Yegor_Khorokhorin/khorokhorinyegor_homework_4.py
@@ -11,7 +11,7 @@ my_list.pop(1)
 print('Ответ списка : ', my_list)
 
 dict_2 = my_dict['dict']
-dict_2['i am a tuple'] = ('six', 1, None, 'text', False)
+dict_2[('i am a tuple',)] = ('six',)
 dict_2.pop('second')
 print('Ответ словаря : ', dict_2)
 
