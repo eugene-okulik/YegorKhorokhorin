c998f5ccae2433205dd48ee05cc4de15e0c27dea
Yegor55 <khorokhorinyegor@gmail.com>
2025-06-16 11:26:06 +0200

    4 files with answers are added to homework_10

---
+++ homework/yegor_khorokhorin/Lesson_10/Lesson_10_homework_Yegor_Khorokhorin/khorokhorinyegor_homework_10_1task.py
@@ -0,0 +1,29 @@
+def finish(func):
+
+    def wrapper():
+        func()
+        print("finished")
+    return wrapper
+
+
+@finish
+def example():
+    print('example')
+
+
+@finish
+def example1():
+    print('anything')
+
+@finish
+def example1():
+    print('anything')
+
+
+@finish
+def example2():
+    print(1+1)
+
+example()
+example1()
+example2()
