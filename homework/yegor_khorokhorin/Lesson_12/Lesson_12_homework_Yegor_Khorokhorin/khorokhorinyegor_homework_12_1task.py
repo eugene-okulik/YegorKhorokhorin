611ce5d695f8b7a0d5ac9225bd0cf1a959afbac2
Yegor55 <khorokhorinyegor@gmail.com>
2025-07-16 11:00:22 +0200

    1 file with fixes is added to homework_12

--- homework/yegor_khorokhorin/Lesson_12/Lesson_12_homework_Yegor_Khorokhorin/khorokhorinyegor_homework_12_1task.py
+++ homework/yegor_khorokhorin/Lesson_12/Lesson_12_homework_Yegor_Khorokhorin/khorokhorinyegor_homework_12_1task.py
@@ -3,54 +3,36 @@ class Flowers:
     root = True
     leaves = True
 
-    def __init__(self, title):
+    def __init__(self, title, color, length_of_stem, time_of_life, price):
         self.title = title
+        self.color = color
+        self.length_of_stem = length_of_stem
+        self.time_of_life = time_of_life
+        self.price = price
+
+    def __str__(self):
+        result = (f"Цветок: {self.title}, цвет: {self.color}, длина стебля: {self.length_of_stem},"
+                  f" время жизни: {self.time_of_life} дней, цена: {self.price}")
+        return result
 
 
 class Rose(Flowers):
     thorns = True
-    time_of_life = 5
-    price = 20
 
-    def __init__(self, title, color, length_of_stem):
-        super().__init__(title)
-        self.color = color
-        self.length_of_stem = length_of_stem
-
-    def __str__(self):
-        result = (f"Цветок: {self.title}, цвет: {self.color}, длина стебля: {self.length_of_stem},"
-                  f" время жизни: {self.time_of_life}дней, цена: {self.price}")
-        return result
+    def __init__(self, title, color, length_of_stem, time_of_life, price):
+        super().__init__(title, color, length_of_stem, time_of_life, price)
 
 
 class Chamomile(Flowers):
-    time_of_life = 2
-    price = 10
 
-    def __init__(self, title, color, length_of_stem):
-        super().__init__(title)
-        self.color = color
-        self.length_of_stem = length_of_stem
-
-    def __str__(self):
-        result = (f"Цветок: {self.title}, цвет: {self.color}, длина стебля: {self.length_of_stem},"
-                  f" время жизни: {self.time_of_life}дня, цена: {self.price}")
-        return result
+    def __init__(self, title, color, length_of_stem, time_of_life, price):
+        super().__init__(title, color, length_of_stem, time_of_life, price)
 
 
 class Cornflower(Flowers):
-    time_of_life = 3
-    price = 5
 
-    def __init__(self, title, color, length_of_stem):
-        super().__init__(title)
-        self.color = color
-        self.length_of_stem = length_of_stem
-
-    def __str__(self):
-        result = (f"Цветок: {self.title}, цвет: {self.color}, длина стебля: {self.length_of_stem},"
-                  f" время жизни: {self.time_of_life}дня, цена: {self.price}")
-        return result
+    def __init__(self, title, color, length_of_stem, time_of_life, price):
+        super().__init__(title, color, length_of_stem, time_of_life, price)
 
 
 class Bunch:
@@ -73,11 +55,8 @@ class Bunch:
         total_price = sum(flower.price for flower in self.flowers)
         return total_price
 
-    def sort_by_color(self):
-        self.flowers.sort(key=lambda flower: flower.color)
-
-    def sort_by_price(self):
-        self.flowers.sort(key=lambda flower: flower.price)
+    def sort_by_value(self, key):
+        self.flowers.sort(key=lambda flower: getattr(flower, key))
 
     def find_by_lifetime(self):
         average = self.average_lifetime()
@@ -88,13 +67,13 @@ class Bunch:
         return result
 
 
-flower_1 = Rose('Rose', 'red', 10)
+flower_1 = Rose('Rose', 'red', 10, 5, 20)
 print(flower_1)
 
-flower_2 = Chamomile('Chamomile', 'white', 5)
+flower_2 = Chamomile('Chamomile', 'white', 5, 2, 10)
 print(flower_2)
 
-flower_3 = Cornflower('Cornflower', 'blue', 3)
+flower_3 = Cornflower('Cornflower', 'blue', 3, 3, 5)
 print(flower_3)
 
 bunch_1 = Bunch() + flower_1 + flower_2 + flower_3
@@ -108,15 +87,15 @@ print(f"Время увядания букета номер 2: {bunch_2.average_
 print(f"Общая стоимость букета номер 2: {bunch_2.bunch_price()}")
 
 
-bunch_1.sort_by_color()
-print("Букет 1 после сортировки по цвету:", bunch_1)
-bunch_2.sort_by_color()
+bunch_1.sort_by_value('length_of_stem')
+print("Букет 1 после сортировки по длине стебля:", bunch_1)
+bunch_2.sort_by_value('color')
 print("Букет 2 после сортировки по цвету:", bunch_2)
 
-bunch_1.sort_by_price()
+bunch_1.sort_by_value('price')
 print("Букет 1 после сортировки по цене:", bunch_1)
 
-bunch_2.sort_by_price()
-print("Букет 2 после сортировки по цене:", bunch_2)
+bunch_2.sort_by_value('time_of_life')
+print("Букет 2 после сортировки по времени жизни:", bunch_2)
 
 flowers_found = bunch_1.find_by_lifetime()
